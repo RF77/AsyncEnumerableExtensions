8b68d54d62822f9dd3fdf10e1e5f0bc63eaecbf9
Rolf Fux <fux@hotmail.ch>
2020-07-05 18:24:56 +0200

    Added comments

--- AsyncEnumerableExtensions/Operators/AsyncStreamQueueExtensions.cs
+++ AsyncEnumerableExtensions/Operators/AsyncStreamQueueExtensions.cs
@@ -6,6 +6,16 @@ namespace System.Linq
 {
 	public static class AsyncStreamQueueExtensions
 	{
+		/// <summary>
+		/// Based on an Enumerable Source a max of defined Calls the functions are made concurrently, but one stream for all returns
+		/// </summary>
+		/// <typeparam name="TResult"></typeparam>
+		/// <typeparam name="TSource"></typeparam>
+		/// <param name="source">Enumeration as input for the functions</param>
+		/// <param name="function">Function to produce a part of the result stream</param>
+		/// <param name="maxConcurrentTasks">Defines how many of the functions are executed concurrently</param>
+		/// <param name="cancellationToken">Cancellation Token</param>
+		/// <returns>A stream containing the results of all calls</returns>
 		public static IAsyncEnumerable<TResult> ForEachAsAsyncEnumerable<TResult, TSource>(
 			this IEnumerable<TSource> source,
 			Func<TSource, IAsyncEnumerable<TResult>> function,
@@ -16,6 +26,16 @@ namespace System.Linq
 			return queue.ExecuteParallelAsync(source.Select(sourceItem => new AsyncStreamQueueItem<TResult, TSource>(sourceItem, function)), maxConcurrentTasks, cancellationToken);
 		}
 
+		/// <summary>
+		/// Based on an async stream source a max of defined Calls the functions are made concurrently, but one stream for all returns
+		/// </summary>
+		/// <typeparam name="TResult"></typeparam>
+		/// <typeparam name="TSource"></typeparam>
+		/// <param name="source">Enumeration as input for the functions</param>
+		/// <param name="function">Function to produce a part of the result stream</param>
+		/// <param name="maxConcurrentTasks">Defines how many of the functions are executed concurrently</param>
+		/// <param name="cancellationToken">Cancellation Token</param>
+		/// <returns>A stream containing the results of all calls</returns>
 		public static IAsyncEnumerable<TResult> ForEachAsAsyncEnumerable<TResult, TSource>(
 			this IAsyncEnumerable<TSource> source,
 			Func<TSource, IAsyncEnumerable<TResult>> function,
