5006d50f7f46b8ea025217ab68b189e6eb677ae0
Rolf Fux <fux@hotmail.ch>
2020-07-08 14:28:33 +0200

    Added MergeSequentially Method for IAsyncEnumerable<IAsyncEnumerable<T>>

--- AsyncEnumerableExtensions.Tests/Operators/Merge.cs
+++ AsyncEnumerableExtensions.Tests/Operators/Merge.cs
@@ -9,32 +9,154 @@
 //  *    RF77 - initial API and implementation and/or initial documentation
 //  *******************************************************************************/
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace AsyncEnumerableExtensions.Tests.Operators
 {
-    public class Merge : UnitTestBase
-    {
-        public Merge(ITestOutputHelper output) : base(output)
-        {
-        }        
-        
-        [Fact]
-        public async Task MergeConcurrentlyOf4Sources()
-        {
-            var firstStream = AsyncEnum.Just(1);
-            var secondStream = AsyncEnum.Just(2);
-            var thirdStream = AsyncEnum.Just(3);
-            var forthStream = AsyncEnum.Just(4);
+	public class Merge : UnitTestBase
+	{
+		public Merge(ITestOutputHelper output) : base(output)
+		{
+		}
 
-            var list = await firstStream.MergeConcurrently(secondStream, thirdStream, forthStream).ToListAsync();
+		private async IAsyncEnumerable<string> GetNumbersAsync(int amount, string prefix,
+			[EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			for (var i = 0; i < amount; i++)
+			{
+				var val = $"{prefix}{i}";
+				//Write($"yield {val}");
+				yield return val;
+				await Task.Delay(10, cancellationToken);
+			}
+		}
 
-            Assert.True(list.Count == 4);
-        }
+		private async IAsyncEnumerable<string> GetNumbersWithExceptionAsync(int amount, string prefix,
+			[EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			for (var i = 0; i < amount; i++)
+			{
+				if (i + 2 == amount)
+				{
+					throw new InvalidOperationException();
+				}
+				var val = $"{prefix}{i}";
+				//Write($"yield {val}");
+				yield return val;
+				await Task.Delay(10, cancellationToken);
+			}
+		}
+
+		[Fact]
+		public async Task MergeConcurrentlyOf4Sources()
+		{
+			var firstStream = AsyncEnum.Just(1);
+			var secondStream = AsyncEnum.Just(2);
+			var thirdStream = AsyncEnum.Just(3);
+			var forthStream = AsyncEnum.Just(4);
+
+			var list = await firstStream.MergeConcurrently(secondStream, thirdStream, forthStream).ToListAsync();
+
+			Assert.True(list.Count == 4);
+		}
+
+		[Fact]
+		public async Task MergeConcurrentlyUntilFirstExceptionOfStreams()
+		{
+			var stream = StreamProducer().MergeConcurrentlyUntilFirstException();
+
+			var list = await stream.ToListAsync();
+
+			Assert.True(list.Count == 25);
+			Assert.True(list.Last() == "E4");
+
+			//await WriteStreamToOutputAsync(stream);
+		}
+
+		[Fact]
+		public async Task MergeConcurrentlyOfStreams()
+		{
+			var stream = StreamProducer().MergeConcurrently();
+
+			var list = await stream.ToListAsync();
+
+			Assert.True(list.Count == 25);
+			Assert.True(list.Last() == "E4");
+
+			//await WriteStreamToOutputAsync(stream);
+		}
+
+		[Fact]
+		public async Task MergeSequentiallyOfStreams()
+		{
+			var stream = StreamProducer().Merge();
+
+			var list = await stream.ToListAsync();
+
+			//Assert.True(list.Count == 25);
+			//Assert.True(list.Last() == "E4");
+
+			await WriteStreamToOutputAsync(stream);
+		}
+
+		[Fact]
+		public async Task MergeConcurrentlyOfStreamsAndCancel()
+		{
+			var cts = new CancellationTokenSource(100);
+
+			var stream = StreamProducer().MergeConcurrentlyUntilFirstException();
+
+			var list = await stream.ToListAsync(cts.Token);
+
+			Assert.True(list.Count < 25);
+
+			//await WriteStreamToOutputAsync(stream);
+		}
+
+		[Fact]
+		public async Task MergeConcurrentlyUntilFristExceptionOfStreamsAndThrowException()
+		{
+			var stream = StreamProducerWithException().MergeConcurrentlyUntilFirstException();
+
+			await Assert.ThrowsAsync<InvalidOperationException>(async () => await stream.ToListAsync());
+
+			//await WriteStreamToOutputAsync(stream);
+		}
 
 
-    }
+		[Fact]
+		public async Task MergeConcurrentlyOfStreamsAndThrowException()
+		{
+			var stream = StreamProducerWithException().MergeConcurrently();
+
+			await Assert.ThrowsAsync<AggregateException>(async () => await stream.ToListAsync());
+
+			//await WriteStreamToOutputAsync(stream);
+		}
+
+		private async IAsyncEnumerable<IAsyncEnumerable<string>> StreamProducer([EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				yield return GetNumbersAsync(5, ((char)('A' + (char) i)).ToString(), cancellationToken);
+				await Task.Delay(30, cancellationToken);
+			}
+		}
+
+		private async IAsyncEnumerable<IAsyncEnumerable<string>> StreamProducerWithException([EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				yield return GetNumbersWithExceptionAsync(5, ((char)('A' + (char)i)).ToString(), cancellationToken);
+				await Task.Delay(30, cancellationToken);
+			}
+		}
+	}
 }
--- AsyncEnumerableExtensions/Operators/MergeOperator.cs
+++ AsyncEnumerableExtensions/Operators/MergeOperator.cs
@@ -5,12 +5,21 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AsyncEnumerableExtensions.Karnok;
+using AsyncEnumerableExtensions.Karnok.impl;
 
 namespace System.Linq
 {
 	public static class MergeOperator
 	{
-		public static IAsyncEnumerable<T> MergeConcurrently<T>(this IAsyncEnumerable<IAsyncEnumerable<T>> sources)
+		/// <summary>
+		/// Merges elements from all inner async-enumerable sequences concurrently into a single async-enumerable sequence.
+		/// Enumeration stops on first thrown Exception of any inner streams.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements in the source sequences.</typeparam>
+		/// <param name="sources">Observable sequence of inner async-enumerable sequences.</param>
+		/// <returns>The async-enumerable sequence that merges the elements of the inner sequences.</returns>
+		/// <exception cref="T:System.ArgumentNullException"><paramref name="sources" /> is null.</exception>
+		public static IAsyncEnumerable<T> MergeConcurrentlyUntilFirstException<T>(this IAsyncEnumerable<IAsyncEnumerable<T>> sources)
 		{
 			var queue = new UnicastAsyncEnumerable<T>();
 			var tasks = new List<Task>();
@@ -61,6 +70,85 @@ namespace System.Linq
 					await queue.Error(e);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Merges elements from all inner async-enumerable sequences concurrently into a single async-enumerable sequence.
+		/// All thrown Exception are thrown at the end of the enumeration of all streams. Multiple Exceptions are thrown as AggregateException.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements in the source sequences.</typeparam>
+		/// <param name="sources">Observable sequence of inner async-enumerable sequences.</param>
+		/// <returns>The async-enumerable sequence that merges the elements of the inner sequences.</returns>
+		/// <exception cref="T:System.ArgumentNullException"><paramref name="sources" /> is null.</exception>
+		public static IAsyncEnumerable<T> MergeConcurrently<T>(this IAsyncEnumerable<IAsyncEnumerable<T>> sources)
+		{
+			var queue = new UnicastAsyncEnumerable<T>();
+			var tasks = new List<Task>();
+			Exception thrownException = null;
+
+			StreamSources();
+
+			return queue;
+
+			async void StreamSources()
+			{
+				try
+				{
+					await foreach (var stream in sources)
+					{
+						tasks.Add(StreamSource(stream));
+					}
+
+					// After the last stream arrived, we have to wait until all of those streams have completely finished with streaming
+					await Task.WhenAll(tasks);
+
+					if (thrownException != null)
+					{
+						await queue.Error(thrownException);
+					}
+				}
+				catch (OperationCanceledException)
+				{
+				}
+				catch (Exception e)
+				{
+					ExceptionHelper.AddException(ref thrownException, e);
+				}
+				finally
+				{
+					await queue.Complete();
+				}
+			}
+
+			async Task StreamSource(IAsyncEnumerable<T> source)
+			{
+				try
+				{
+					await foreach (var item in source)
+					{
+						await queue.Next(item);
+					}
+				}
+				catch (OperationCanceledException)
+				{
+				}
+				catch (Exception e)
+				{
+					ExceptionHelper.AddException(ref thrownException, e); ;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Merges elements from all inner async-enumerable sequences sequentially into a single async-enumerable sequence.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements in the source sequences.</typeparam>
+		/// <param name="sources">Observable sequence of inner async-enumerable sequences.</param>
+		/// <returns>The async-enumerable sequence that merges the elements of the inner sequences.</returns>
+		/// <exception cref="T:System.ArgumentNullException"><paramref name="sources" /> is null.</exception>
+		public static IAsyncEnumerable<T> MergeSequentially<T>(this IAsyncEnumerable<IAsyncEnumerable<T>> sources)
+		{
+			return sources.Merge();
 		}
 	}
 }
