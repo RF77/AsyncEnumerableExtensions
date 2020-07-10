304f95e107f3f913156be7bce2629e0edc27ccc7
Rolf Fux <fux@hotmail.ch>
2020-07-10 19:32:52 +0200

    Added TimeStampItem

---
+++ AsyncEnumerableExtensions/TimeOperators/TimeStampItem.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2020 by RF77 (https://github.com/RF77)
+// Licensed under the Apache 2.0 License.
+// See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace AsyncEnumerableExtensions.TimeOperators
+{
+	/// <summary>
+	/// Any type of item with an timestamp
+	/// </summary>
+	public class TimeStampItem<T>
+	{
+		public TimeStampItem(DateTime timeStamp, T item)
+		{
+			TimeStamp = timeStamp;
+			Item = item;
+		}
+
+		public DateTime TimeStamp { get; }
+
+		public T Item { get; }
+	}
+}
---
+++ AsyncEnumerableExtensions/TimeOperators/TimeStampItemExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2020 by RF77 (https://github.com/RF77)
+// Licensed under the Apache 2.0 License.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using AsyncEnumerableExtensions.TimeOperators;
+
+namespace System.Linq
+{
+	public static class TimeStampItemExtensions
+	{
+		/// <summary>
+		/// Consumes a stream and and add a time stamp with the current time (local time)
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="stream"></param>
+		/// <returns></returns>
+		public static IAsyncEnumerable<TimeStampItem<T>> AddCurrentTime<T>(this IAsyncEnumerable<T> stream)
+		{
+			return stream.Select(i => new TimeStampItem<T>(DateTime.Now, i));
+		}
+
+		/// <summary>
+		/// Converts a stream back to items only (removes the timestamp)
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="stream"></param>
+		/// <returns></returns>
+		public static IAsyncEnumerable<T> RemoveTimeStamp<T>(this IAsyncEnumerable<TimeStampItem<T>> stream)
+		{
+			return stream.Select(i => i.Item);
+		}
+	}
+}
